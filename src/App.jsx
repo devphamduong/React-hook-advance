0a2578a00a850c69cb57146161786bd95da05c02
Pham Chu Duong <pduong244@gmail.com>
2023-01-17 21:54:13 +0700

    Data flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css';
 import NavBar from './components/Header/NavBar';
 import Content from './components/Main/Content';
 import Lesson3 from './lessons/Lesson3/Lesson3';
 import Footer from './components/Footer/Footer';
+import Lesson4 from './lessons/Lesson4/Lesson4';
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -15,10 +16,11 @@ const App = () => {
         count={count}
         setCount={setCount}
       /> */}
-      <Lesson3 />
+      {/* <Lesson3 /> */}
+      <Lesson4 />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/lessons/Lesson3/Lesson3.jsx
+++ src/lessons/Lesson3/Lesson3.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react';
 import CoolButton from './CoolButton';
 
 const Lesson3 = (props) => {
-    const [name, setName] = useState('')
+    const [name, setName] = useState('');
     return (
         <div>
             <label>Name:</label>
-            <input type={'text'} value={name} onChange={(event)=>setName(event.target.value)}/>
+            <input type={'text'} value={name} onChange={(event) => setName(event.target.value)} />
             <div>Your name 's {name}</div>
-            <CoolButton btnClass={'primary'} title={'Press me'}/>
+            <CoolButton btnClass={'primary'} title={'Press me'} />
         </div>
-    )
-}
+    );
+};
 
 export default Lesson3;
