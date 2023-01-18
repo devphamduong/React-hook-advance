df809561e2c0edeb1d1ef728d4e7e6addd780707
Pham Chu Duong <pduong244@gmail.com>
2023-01-18 15:45:16 +0700

    normal assign/copy

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -4,6 +4,7 @@ import 'nprogress/nprogress.css';
 import Lesson17 from "./chapter3/Lesson17";
 import Lesson18 from "./chapter3/Lesson18";
 import Lesson19 from "./chapter3/Lesson19";
+import Lesson20 from "./chapter3/Lesson20";
 // import Lesson26 from "./chapter4/lesson26/Lesson26";
 
 const Project = (props) => {
@@ -12,7 +13,8 @@ const Project = (props) => {
             {/* <Login /> */}
             {/* <Lesson17 /> */}
             {/* <Lesson18 /> */}
-            <Lesson19 />
+            {/* <Lesson19 /> */}
+            <Lesson20 />
             {/* <Lesson26 /> */}
         </div>
     );
