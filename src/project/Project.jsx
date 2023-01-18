78ec9f8436a6952a6282887090701fdf6021307d
Pham Chu Duong <pduong244@gmail.com>
2023-01-18 15:30:19 +0700

    Convert to a Boolean

--- src/project/Project.jsx
+++ src/project/Project.jsx
@@ -1,15 +1,17 @@
 import Login from "./Login";
 import './Project.scss';
 import 'nprogress/nprogress.css';
-// import Lesson26 from "./chapter4/lesson26/Lesson26";
 import Lesson17 from "./chapter3/Lesson17";
+import Lesson18 from "./chapter3/Lesson18";
+// import Lesson26 from "./chapter4/lesson26/Lesson26";
 
 const Project = (props) => {
     return (
         <div className="project">
-            <Login />
+            {/* <Login /> */}
+            {/* <Lesson17 /> */}
+            <Lesson18 />
             {/* <Lesson26 /> */}
-            <Lesson17 />
         </div>
     );
 };
